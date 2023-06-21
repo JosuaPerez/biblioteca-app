61425058b14ee679798e65a2a5c04cc8b23188df
Josua Perez <josuap63@gmail.com>
2023-06-21 19:27:16 -0400

    minor changes

--- database/factories/CategoryFactory.php
+++ database/factories/CategoryFactory.php
@@ -18,7 +18,7 @@ class CategoryFactory extends Factory
     public function definition(): array
     {
         return [
-            'category_name' => fake()->name(),
+            'category_name' => fake()->word(),
         ];
     }
 }
